3d78f8f139aa1559b04b1f0e3d045e1320e0a309
Daniel Ontiveros <cardanont@gmail.com>
2020-10-15 23:59:06 -0500

    Simple middleware tests

---
+++ demo1/Middleware/SimpleMiddleware1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo1.Middleware
+{
+    public class SimpleMiddleware1 : IMiddleware
+    {
+        public async Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            await context.SendActivityAsync($"[SimpleMiddleware1] (context)/OnTurn/Before");
+        }
+    }
+}
