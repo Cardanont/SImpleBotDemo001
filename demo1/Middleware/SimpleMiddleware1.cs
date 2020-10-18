3c98c145ccae81e468e96caffe13afc2409af603
Daniel Ontiveros <cardanont@gmail.com>
2020-10-17 21:06:48 -0500

    NextDelegate throwing errors investigating how to fix this, maybe change on the version for core 3.1

--- demo1/Middleware/SimpleMiddleware1.cs
+++ demo1/Middleware/SimpleMiddleware1.cs
@@ -2,15 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace demo1.Middleware
 {
     public class SimpleMiddleware1 : IMiddleware
     {
-        public async Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        //public async Task OnTurn(ITurnContext context, NextDelegate next)
+        //{
+            
+        //}
+
+        public async Task OnTurnAsync(ITurnContext context, NextDelegate next, CancellationToken cancellationToken = default)
         {
-            await context.SendActivityAsync($"[SimpleMiddleware1] (context)/OnTurn/Before");
+            await context.SendActivityAsync($"[SimpleMiddleware1] {context.Activity.Type}/OnTurn/Before");
+
+            await next();
+
+            await context.SendActivityAsync($"[SimpleMiddleware] {context.Activity.Type}/OnTurn/After");
         }
     }
 }
